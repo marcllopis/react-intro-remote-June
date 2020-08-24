c474b64d39eb252573e8c26aa972cf8067be36c6
Marc <marcllopis89@gmail.com>
2020-08-24 20:29:55 +0200

    Creates the react app with some components as example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <div className="content">
+        <div className="student-container">
+          <h1>Roberto</h1>
+          <h2>From Romania</h2>
+          <img
+            alt="roberto"
+            className="img-student"
+            src="https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg" />
+        </div>
+        <div className="student-container">
+          <h1>Carlos</h1>
+          <h2>From Portugal</h2>
+          <img
+            alt="roberto"
+            className="img-student"
+            src="https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg" />
+        </div>
+        <div className="student-container">
+          <h1>Pablo</h1>
+          <h2>From Argentina</h2>
+          <img
+            alt="roberto"
+            className="img-student"
+            src="https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg" />
+        </div>
+        <div className="student-container">
+          <h1>Adam</h1>
+          <h2>From Romania</h2>
+          <img
+            alt="roberto"
+            className="img-student"
+            src="https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg" />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Navbar() {
+  return (
+    <div className="nav">
+      My 1st project
+    </div>
+  )
+}
+
+
+export default Navbar;
